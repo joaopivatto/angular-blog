58e81553505772194e16dd8e6d3fd50cd33f0a91
Joao Pivatto <jgpivatto@gmail.com>
2025-05-10 06:46:16 -0300

    initial blog finished

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,32 @@
+export const dataFake = [
+	{
+		"id": "1",
+		"photoCover": "https://ionic.io/blog/wp-content/uploads/2024/02/angular-feature-image-1.png",
+		"title": "New Angular Version",
+		"description": "The new Angular version brings cutting-edge features, improved performance, and better developer experience for building scalable web applications."
+	},
+	{
+		"id": "2",
+		"photoCover": "https://bfnetworks.com.br/wp-content/uploads/2024/09/docker.png",
+		"title": "How to use Docker",
+		"description": "This guide explains how to get started with Docker, including creating containers, managing images, and integrating it into your development workflow."
+	},
+	{
+		"id": "3",
+		"photoCover": "https://rdrblog.com.br/wp-content/uploads/2020/08/Spring-BOOT-Interview-questions-1.jpg",
+		"title": "Why should you use Java with SpringBoot",
+		"description": "Java and Spring Boot together offer a powerful combination for building robust, fast, and production-ready backend services with minimal configuration."
+	},
+	{
+		"id": "4",
+		"photoCover": "https://zup.com.br/wp-content/uploads/2021/03/5ce2fde702ef93c1e994d987_flutter.png",
+		"title": "How can Flutter help",
+		"description": "Flutter helps by allowing you to build beautiful, natively compiled apps for mobile, web, and desktop from a single codebase, saving time and effort."
+	},
+	{
+		"id": "5",
+		"photoCover": "https://img.freepik.com/vetores-gratis/circulo-azul-com-usuario-branco_78370-4707.jpg?semt=ais_hybrid&w=740",
+		"title": "My Projects",
+		"description": "Please, acess my github by clicking the menubar shortcut to see my projects."
+	}
+];
